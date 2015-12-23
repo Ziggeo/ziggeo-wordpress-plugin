f4cfb2ecb1938dbc43911ed6b7c2e66ce0d9348c
Oliver Friedmann <oliver.friedmann@gmail.com>
2015-12-23 16:32:28 -0500

    Allow video posts from namespaced urls

--- src/templates.php
+++ src/templates.php
@@ -1,12 +1,17 @@
 <?php
 
-if ($_SERVER["PHP_SELF"] == "/wp-admin/post-new.php" || $_SERVER["PHP_SELF"] == "/wp-admin/edit.php" || $_SERVER["PHP_SELF"] == "/wp-admin/post.php") 
+function ziggeo_url_endswith($needle) {
+	global $_SERVER;
+	$haystack = $_SERVER["PHP_SELF"];
+	return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle, $temp) !== FALSE);
+}
+
+if (ziggeo_url_endswith("/wp-admin/post-new.php") || ziggeo_url_endswith("/wp-admin/edit.php") || ziggeo_url_endswith($_SERVER["PHP_SELF"])) 
 	add_action('admin_head', "ziggeo_post_template");
 	
 function ziggeo_post_template() {
 	include(dirname(__FILE__) . "/../templates/post_template.php");
 }
- 
 
 function ziggeo_comment_template($comment_template) {
 	return dirname(__FILE__) . "/../templates/comments_template.php";
