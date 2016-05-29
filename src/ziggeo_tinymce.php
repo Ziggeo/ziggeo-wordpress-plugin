82c041d7d4ab1d65671f7655e2a639612a84ccae
Bane-D <baned.ben@gmail.com>
2016-05-30 01:10:40 +0200

    - hide TinyMCE button if desired option added

--- src/ziggeo_tinymce.php
+++ src/ziggeo_tinymce.php
@@ -2,10 +2,21 @@
 	//All code related to tinyMCE is stored here
 	// This file is included from within ziggeo.php file
 
-	//Registering our plugin in the list of TinyMCE list of external plugins
-	add_filter('mce_external_plugins', 'ziggeo_mce_register');
-	//Adding a button to the TinyMCE toolbar
-	add_filter('mce_buttons', 'ziggeo_mce_add_button');
+defined('ABSPATH') or die();
+
+//To allow us to see if people want to have the TinyMCE button shown in the toolbar or not. If not, no need to show the same.
+function ziggeo_check_mce() {
+
+	$options = get_option('ziggeo_video'); 
+
+	if( (isset($options['showVideoAidButton']) && $options['showVideoAidButton'] === '1') || !isset($options['showVideoAidButton']) ) {
+		//Registering our plugin in the list of TinyMCE list of external plugins
+		add_filter('mce_external_plugins', 'ziggeo_mce_register');
+		//Adding a button to the TinyMCE toolbar
+		add_filter('mce_buttons', 'ziggeo_mce_add_button');		
+	}
+}
+ziggeo_check_mce();
 
 //We must register the URL to our plugin in TinyMCE, and we do that here
 function ziggeo_mce_register($plugin_array) {
